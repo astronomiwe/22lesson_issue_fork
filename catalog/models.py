a2ea76c608aeb86fd329b906c754a04446dba2fd
VladimirKovaliev <kovaliev.v@list.ru>
2023-12-03 18:52:17 +0300

    fixes

--- catalog/models.py
+++ catalog/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Product(models.Model):
 
     name = models.CharField(max_length=100, verbose_name='наименование')
-    product_discription = models.TextField(verbose_name='описание')
+    discription = models.TextField(verbose_name='описание')
     preview = models.ImageField(upload_to='products/', verbose_name='превью', null=True, blank=True)
     category = models.CharField(max_length=100, verbose_name='категория')
     price = models.IntegerField(verbose_name='цена за штуку')
-    date_create = models.DateField(verbose_name='дата создания')
-    date_last_change = models.DateField(verbose_name='дата последнего изменения', null=True, blank=True)
+    date_create = models.DateField(auto_now=True)
+    date_last_change = models.DateField(auto_now=True)
 
     def __str__(self):
         return f'{self.name}, {self.product_discription}, {self.price}'
