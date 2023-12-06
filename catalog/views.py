23c5730e8ffbff310ccd25c025ef1b71564bb1c3
VladimirKovaliev <kovaliev.v@list.ru>
2023-12-06 21:29:22 +0300

    fixes 20.2

--- catalog/views.py
+++ catalog/views.py
@@ -5,15 +5,15 @@ from catalog.models import Category, Product
 
 def index(request):
     context = {
-        'object_list': Category.object.all(),
+        'object_list': Category.objects.all(),
         'title': 'SkyStore - Главная'
     }
-    return render(request, 'products/index.html', context)
+    return render(request, 'catalog/index.html', context)
 
 
 def categories(request):
     context = {
-        'object_list': Category.object.all(),
+        'object_list': Category.objects.all(),
         'title': 'SkyStore - Все наши товары'
     }
     return render(request, 'products/categories.html', context)
