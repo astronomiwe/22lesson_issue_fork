4963b9cd24e06b085b697eedd643cab1ccaea755
VladimirKovaliev <kovaliev.v@list.ru>
2023-12-17 15:46:51 +0300

    add urls and CBV

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,50 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from pytils.translit import slugify
+
+from blog.models import Post
 
 
 class PostListView(ListView):
-    pass
+    model = Post
+    extra_context = {'title': 'Posts'}
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(is_published=True)
+        return qs
 
 
 class PostCreateView(CreateView):
-    pass
+    model = Post
+    fields = ('title', 'content', 'preview', 'is_published')
+    success_url = reverse_lazy('blog:post_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.title)
+            new_mat.save()
+        return super().form_valid(form)
 
 
 class PostDetailView(DetailView):
-    pass
+    model = Post
+
+    def get_object(self, queryset=None):
+        new_mat = super().get_object(queryset)
+        new_mat.views_count += 1
+        new_mat.save()
+        return new_mat
+
 
 class PostUpdateView(UpdateView):
-    pass
+    model = Post
+    fields = ('title', 'content', 'preview', 'is_published')
+    success_url = reverse_lazy('blog:post_list')
 
 
 class PostDeleteView(DeleteView):
-    pass
+    model = Post
+    success_url = reverse_lazy('blog:post_list')
