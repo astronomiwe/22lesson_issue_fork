eca841d02f250d659e4b79812bb8af467a5a95ab
VladimirKovaliev <kovaliev.v@list.ru>
2023-12-25 17:05:31 +0300

    fix login path

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,7 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    # path('', LoginView.as_view(template_name='users/login.html'), name='login'),
+    #path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('accounts/logout/', views.logout_view, name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
